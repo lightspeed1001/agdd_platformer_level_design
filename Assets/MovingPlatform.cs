7810d0b0337373fca91369fc4669c2f5ca6b7322
Mikael <lightspeed1001@gmail.com>
2020-03-03 16:35:32 +0000

    initial commit

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AGDDPlatformer
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        public Collider2D triggerCollider;
+        public Rigidbody2D rb;
+        public GameObject target;
+        public Vector3 deltaPosition;
+        public bool continuous = false;
+        public bool startActive = false;
+        public float speed = 0.5f;
+        public float edgeWait = 0.5f;
+
+        private bool isActive;
+        private Vector3 startingLocation;
+        private Vector3 targetLocation;
+        private float edgeTimer;
+        private bool movingToStart = false;
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            isActive = startActive;
+            startingLocation = transform.position;
+            targetLocation = startingLocation + deltaPosition;
+            edgeTimer = edgeWait;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if(!isActive) return;
+
+            if(movingToStart)
+            {
+                // Debug.Log("going to start");
+                transform.position = Vector3.MoveTowards(transform.position, startingLocation, speed);
+                if(transform.position == startingLocation)
+                {
+                    movingToStart = false;
+                    isActive = false;
+                }
+                return;
+            }
+            else
+            {
+                // Debug.Log("going somewhere");
+                Vector3 newPos = Vector3.MoveTowards(transform.position, targetLocation, speed);
+                // Debug.Log(newPos);
+                Vector3 diff = newPos - transform.position;
+                rb.MovePosition(newPos);
+                if(target != null)
+                    target.transform.position += diff;
+                // transform.position = Vector3.MoveTowards(transform.position, targetLocation, speed);
+            }
+            
+            if(transform.position == targetLocation && !continuous)
+            {
+                isActive = false;
+                return;
+            }
+            
+            if(continuous && targetLocation == transform.position && (edgeTimer = edgeTimer - Time.deltaTime) <= 0.0f)
+            {
+                Vector3 tmp = startingLocation;
+                startingLocation = targetLocation;
+                targetLocation = tmp;
+                edgeTimer = edgeWait;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.gameObject.CompareTag("Player1"))
+            {
+                target = other.gameObject;
+                // other.transform.parent = transform;
+                // other.transform.position = triggerCollider.transform.position + new Vector3(0, 0.25f, 0);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(other.gameObject.CompareTag("Player1")) //&& other.gameObject.transform.parent == this.transform)
+            {
+                target = null;
+                // other.transform.parent = null;
+            }
+        }
+
+        public void StartMoving()
+        {
+            isActive = true;
+        }
+
+        public void MoveToStartingPosition()
+        {
+            movingToStart = true;
+            StartMoving();
+        }
+    }
+}
---
+++ Assets/SwitchObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AGDDPlatformer
+{
+    public class SwitchObject : MonoBehaviour
+    {
+        public SpriteRenderer sr;
+        public Color pressedColor;
+        public Color timerColor;
+        public MovingPlatform targetPlatform;
+        public bool timedSwitch = false;
+        public float maximumActiveTime = 0.0f;
+        private bool pressed = false;
+        private float timer;
+        private Color startingColor;
+
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            startingColor = sr.color;
+            timer = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!pressed || !timedSwitch) return;
+
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                targetPlatform.MoveToStartingPosition();
+                pressed = false;
+                sr.color = startingColor;
+            }
+        }
+
+        /// <summary>
+        /// Sent when another object enters a trigger collider attached to this
+        /// object (2D physics only).
+        /// </summary>
+        /// <param name="other">The other Collider2D involved in this collision.</param>
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("Player1"))
+            {
+                pressed = true;
+                // Debug.Log("Movng platform");
+                targetPlatform.StartMoving();
+                timer = maximumActiveTime;
+                if(timedSwitch)
+                    sr.color = timerColor;
+                else
+                    sr.color = pressedColor;
+            }
+        }
+    }
+}
